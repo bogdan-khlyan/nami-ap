4b4725f0c8b9f99e267a56181e851601f64580e7
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-10-05 20:08:23 +0300

    fix(ProductEditor.vue): added preloader

--- src/components/pages/productEditor/ProductEditor.vue
+++ src/components/pages/productEditor/ProductEditor.vue
@@ -2,7 +2,7 @@
   <div class="product-editor">
     <base-card>
       <template #header>
-        <span>Редактирование продукта</span>
+        <span>{{ isCreate ? 'Создание' : 'Редактирование' }} продукта</span>
         <el-button
             class="product-editor__header--btn"
             type="primary" icon="check"
@@ -10,74 +10,80 @@
         >Сохранить</el-button>
       </template>
 
-      <template v-if="product">
-        <base-label label="Тим продукта" tag="span">
-          <el-radio-group v-model="product.type" :disabled="!isCreate">
-            <el-radio label="SINGLE" size="large">Обычный</el-radio>
-            <el-radio label="VARIANT" size="large">С вариантами начинок</el-radio>
-          </el-radio-group>
-        </base-label>
-        <div v-if="product.type === 'SINGLE'"
-             class="product-editor__images">
-          <base-upload-images v-model="product.images"/>
-        </div>
-        <div class="product-editor__content">
-          <base-card class="product-editor__card">
-            <template #header>Основная информация</template>
-            <base-label tag="span">
-              <el-select
-                  v-model="product.categories"
-                  multiple
-                  collapse-tags
-                  collapse-tags-tooltip
-                  placeholder="Категории"
-              >
-                <el-option
-                    v-for="item in categories"
-                    :key="item._id"
-                    :label="item.title"
-                    :value="item._id"
+      <transition name="el-fade-in-linear" mode="out-in">
+        <div v-if="product">
+          <base-label label="Тим продукта" tag="span">
+            <el-radio-group v-model="product.type" :disabled="!isCreate">
+              <el-radio label="SINGLE" size="large">Обычный</el-radio>
+              <el-radio label="VARIANT" size="large">С вариантами начинок</el-radio>
+            </el-radio-group>
+          </base-label>
+          <div v-if="product.type === 'SINGLE'"
+               class="product-editor__images">
+            <base-upload-images v-model="product.images"/>
+          </div>
+          <div class="product-editor__content">
+            <base-card class="product-editor__card">
+              <template #header>Основная информация</template>
+              <base-label tag="span">
+                <el-select
+                    v-model="product.categories"
+                    multiple
+                    collapse-tags
+                    collapse-tags-tooltip
+                    placeholder="Категории"
+                >
+                  <el-option
+                      v-for="item in categories"
+                      :key="item._id"
+                      :label="item.title"
+                      :value="item._id"
+                  />
+                </el-select>
+              </base-label>
+              <base-label :validator="v$.product.title">
+                <el-input v-model="product.title" placeholder="Наименование продукта" />
+              </base-label>
+              <base-label :validator="v$.product.description">
+                <el-input
+                    v-model="product.description"
+                    :rows="2"
+                    type="textarea"
+                    placeholder="Описание продукта"
                 />
-              </el-select>
-            </base-label>
-            <base-label :validator="v$.product.title">
-              <el-input v-model="product.title" placeholder="Наименование продукта" />
-            </base-label>
-            <base-label :validator="v$.product.description">
-              <el-input
-                  v-model="product.description"
-                  :rows="2"
-                  type="textarea"
-                  placeholder="Описание продукта"
+              </base-label>
+              <div v-if="product.type === 'SINGLE'"
+                   class="product-editor__cost">
+                <base-label label="Стоимость">
+                  <el-input-number v-model="product.cost" :min="0" />
+                </base-label>
+                <base-label label="Вес">
+                  <el-input-number v-model="product.weight" :min="0" />
+                </base-label>
+              </div>
+            </base-card>
+            <base-card class="product-editor__card">
+              <template #header>Ингридиенты</template>
+              <ingredients v-model="product.ingredients"/>
+            </base-card>
+            <base-card
+                v-if="product.type === 'VARIANT'"
+                class="product-editor__card"
+                style="width: 100%"
+            >
+              <template #header>Начинки</template>
+              <variants
+                  :current-variants="product.variants"
+                  ref="variants"
               />
-            </base-label>
-            <div v-if="product.type === 'SINGLE'"
-                 class="product-editor__cost">
-              <base-label label="Стоимость">
-                <el-input-number v-model="product.cost" :min="0" />
-              </base-label>
-              <base-label label="Вес">
-                <el-input-number v-model="product.weight" :min="0" />
-              </base-label>
-            </div>
-          </base-card>
-          <base-card class="product-editor__card">
-            <template #header>Ингридиенты</template>
-            <ingredients v-model="product.ingredients"/>
-          </base-card>
-          <base-card
-              v-if="product.type === 'VARIANT'"
-              class="product-editor__card"
-              style="width: 100%"
-          >
-            <template #header>Начинки</template>
-            <variants
-                :current-variants="product.variants"
-                ref="variants"
-            />
-          </base-card>
+            </base-card>
+          </div>
         </div>
-      </template>
+        <div v-else
+             v-loading="true"
+             class="product-editor__loading"
+        />
+      </transition>
     </base-card>
   </div>
 </template>
@@ -143,10 +149,10 @@ export default {
     },
     async save() {
       this.v$.$touch()
-      if (!this.v$.$error && this.$refs.variants.validate()) {
+      if (!this.v$.$error) {
         if (this.product.type === 'SINGLE') {
           await this.saveSingle()
-        } else {
+        } else if (this.$refs.variants.validate()) {
           await this.saveVariant()
         }
       }
@@ -252,6 +258,9 @@ export default {
     :deep(.base-label) {
       width: 100%;
     }
+  }
+  &__loading {
+    height: 400px;
   }
 }
 </style>
