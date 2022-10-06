416f873cc41ad053c5d1839af241821826b68eb8
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-10-06 13:54:36 +0300

    fix: auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [{
-  path: '/',
+  path: '/login',
   name: 'login',
   component: () => import('@/components/pages/login/Login')
 }, {
