4ce687fd802504e955c7ba132b4278a666d95137
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-10-06 20:08:44 +0300

    fix(Ingredients.vue): added validate

--- src/components/pages/productEditor/components/Ingredients.vue
+++ src/components/pages/productEditor/components/Ingredients.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="ingredients">
     <div class="ingredients__item"
-         v-for="(item, index) in modelValue">
-      <el-input v-model="modelValue[index]" placeholder="Наименование" />
+         v-for="(item, index) in modelValue"
+         :ref="`ingredient${index}`">
+      <el-input
+          v-model="modelValue[index]"
+          placeholder="Наименование"
+          @input="validateItem(index)"
+      />
       <el-button
           type="danger" icon="delete"
           @click="deleteItem(index)"
@@ -21,7 +26,34 @@ export default {
   props: {
     modelValue: { type: Array }
   },
+  data() {
+    return {
+      touch: false
+    }
+  },
   methods: {
+    validate() {
+      this.touch = true
+      let valid = true
+      for (let i in this.modelValue) {
+        if (!this.validateItem(i)) {
+          valid = false
+        }
+      }
+      return valid
+    },
+    validateItem(index) {
+      if (!this.touch) {
+        return
+      }
+      if (!this.modelValue[index] || this.modelValue[index].length <= 3) {
+        this.$refs[`ingredient${index}`][0].classList.add('error')
+        return false
+      } else {
+        this.$refs[`ingredient${index}`][0].classList.remove('error')
+        return true
+      }
+    },
     deleteItem(index) {
       this.modelValue.splice(index, 1)
     }
@@ -33,8 +65,13 @@ export default {
 .ingredients {
   &__item {
     display: flex;
-    margin-top: 2px;
-    margin-bottom: 2px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    &.error {
+      :deep(.el-input__wrapper) {
+        box-shadow: 0 0 0 1px red!important;
+      }
+    }
     :deep(.el-button) {
       margin-left: 5px;
     }
