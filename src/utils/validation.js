ae2e1d9944cddaf1cf0b60dfce8a95a475075966
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-10-07 23:14:49 +0300

    fix(validation.js): added maxLength message

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,5 @@
 export const messagesMap = new Map()
     .set('required', () => 'Не может быть пустым')
     .set('minLength', ({ $params }) => `Минимум ${$params.min} символов`)
+    .set('maxLength', ({ $params }) => `Максимум ${$params.max} символов`)
     .set('minValue', ({ $params }) => `Значение не может быть меньше ${$params.min}`)
