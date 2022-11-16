45621e2b72a3f8fe34d3d42da234f5784e9d6249
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-11-16 14:36:39 +0300

    fix(links.js): comment dashboard

--- src/components/common/baseWrapper/links.js
+++ src/components/common/baseWrapper/links.js
@@ -1,7 +1,7 @@
 export const links = [
-    { title: 'Главная', icon: 'icon-dashboard', to: '/dashboard' },
+    // { title: 'Главная', icon: 'icon-dashboard', to: '/dashboard' },
     { title: 'Пользователи', icon: 'icon-user', to: '/users' },
     { title: 'Продукты', icon: 'icon-product', to: '/products' },
-    { title: 'Категории', icon: 'icon-grid', to: '/categories' },
+    { title: 'Категории', icon: 'icon-dashboard', to: '/categories' },
     { title: 'Иконки', icon: 'icon-grid', to: '/icons' },
 ]
