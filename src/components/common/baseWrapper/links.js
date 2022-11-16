b82ff391a09436b7aa799250078b444b0eafed36
Den <denkalinovsky@gmail.com>
2022-11-16 18:46:09 +0300

    chore(links.js): add new link - 'Orders'

--- src/components/common/baseWrapper/links.js
+++ src/components/common/baseWrapper/links.js
@@ -1,6 +1,7 @@
 export const links = [
     // { title: 'Главная', icon: 'icon-dashboard', to: '/dashboard' },
     { title: 'Пользователи', icon: 'icon-user', to: '/users' },
+    { title: 'Заказы', icon: 'icon-product', to: '/orders' },
     { title: 'Продукты', icon: 'icon-product', to: '/products' },
     { title: 'Категории', icon: 'icon-dashboard', to: '/categories' },
     { title: 'Иконки', icon: 'icon-grid', to: '/icons' },
